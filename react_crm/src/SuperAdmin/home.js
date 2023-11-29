167a00c9fc79322bf071df959c04d86fea54973d
Barish-S <barishs28@gmail.com>
2023-11-29 14:15:51 +0530

    html to js

---
+++ react_crm/src/SuperAdmin/home.js
@@ -0,0 +1,37 @@
+import './home.css';
+
+function homeAdmin() {
+    return (
+        <div class="header">
+            <ul id="nav">
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+            <div class="side-nav">
+                <div class="user">
+                    <img src="images/user.png" class="user-img" />
+                    <div>
+                        <h2>Barish</h2>
+                        <p>barishs28@gmail.com</p>
+                    </div>
+                    <img src="images/star.png" class="star-img" />
+                </div>
+                <ul>
+                    <li><img src="images/dashboard.png" /><p>Dashboard</p></li>
+                    <li><img src="images/reports.png" /><p>Reports</p></li>
+                    <li><img src="images/projects.png" /><p>Clients</p></li>
+                    <li><img src="images/members.png" /><p>Employees</p></li>
+                    <li><img src="images/rewards.png" /><p>Works</p></li>
+                    <li><img src="images/messages.png"/><p>Messages</p></li>
+                    <li><img src="images/setting.png" /><p>Assign Users</p></li>
+                </ul>
+                <ul>
+                    <li><img src="images/logout.png" /><p>Logout</p></li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default homeAdmin
---
+++ react_crm/src/UserLogin/home.js
@@ -0,0 +1,33 @@
+import './home.css';
+
+function homeUser() {
+    return (
+    <div class="header">
+        <ul id="nav">
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+        <div class="side-nav">
+            <div class="user">
+                <img src="images/user.png" class="user-img"/>
+                <div>
+                    <h2>Arun</h2>
+                    <p>arun@gmail.com</p>
+                </div>
+                <img src="images/star.png" class="star-img"/>
+            </div>
+            <ul>
+                <li><img src="images/reports.png"/><p>Reports</p></li>
+                <li><img src="images/projects.png"/><p>Client</p></li>
+                <li><img src="images/rewards.png"/><p>Work</p></li>
+                <li><img src="images/messages.png"/><p>Messages</p></li>
+            </ul>
+            <ul>
+                <li><img src="images/logout.png"/><p>Logout</p></li>
+            </ul>
+        </div>
+    </div>
+    )
+}
+export default homeUser
