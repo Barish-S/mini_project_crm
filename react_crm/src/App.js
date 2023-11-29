59822028124ac0b597778cae14e61ac11c1a9ce8
bhuva-na <rizanabhuvana@gmail.com>
2023-11-29 13:57:32 +0530

    merge change

--- react_crm/src/App.js
+++ react_crm/src/App.js
@@ -1,23 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-<<<<<<<<< Temporary merge branch 1
 import Submit from './login';
 import Divert from './Homepage/homepage';
 import {
   createBrowserRouter,
   RouterProvider,
-} from "react-router-dom"; <img src={logo} className="App-logo" alt="logo" />
-<p>
-  Edit <code>src/App.js</code> and save to reload.
-</p>
-<a
-  className="App-link"
-  href="https://reactjs.org"
-  target="_blank"
-  rel="noopener noreferrer"
->
-  Learn React
-</a>
+} from "react-router-dom"; 
 import Submit from './login';
 import Divert from './Homepage/homepage';
 function App() {
@@ -40,24 +28,11 @@ function App() {
   return ( 
     <div className="App">
       <header className="App-header">
-<<<<<<<<< Temporary merge branch 1
       <RouterProvider router={router} />
-=========
-        {/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
+       
 
         <ClientRegister />
->>>>>>>>> Temporary merge branch 2
+
 
       </header>
     
