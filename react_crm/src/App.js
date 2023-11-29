c8c492f589ea42ab17990f8ec6b3216a737e186b
bhuva-na <rizanabhuvana@gmail.com>
2023-11-29 13:46:14 +0530

    again push

--- react_crm/src/App.js
+++ react_crm/src/App.js
@@ -1,6 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Submit from './login';
+import Divert from './Homepage/homepage';
 function App() {
   const router = createBrowserRouter([
     {
@@ -21,20 +26,9 @@ function App() {
   return ( 
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <RouterProvider router={router}/>
       </header>
-     
+    
     </div>
   );
   }
