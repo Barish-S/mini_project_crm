a29f9568ab8d966daa0d073ac4970457918ec18b
Chrish303 <christhurajan333@gmail.com>
2023-12-19 15:08:17 +0530

    client

--- react_crm/src/reducer/userSlice.js
+++ react_crm/src/reducer/userSlice.js
@@ -22,12 +22,23 @@ export const userSlice = createSlice({
   reducers: {
     EmployeeRegisterDetails : (state,action) => {
       state.EmpRegDet = action.payload
+    },
+    RegisterClient : (state,action) => {
+      state.ClientDetails = action.payload
+    },
+    UpdateUserLoginSuccess : (state,action) => {
+      state.userloginsuccess = action.payload
+    },
+   setClientLogin : (state,action) => {
+      state.clientLogin = action.payload
+    },
+    superAdminLogin : (state,action) => {
+      state.superAdminLogin = action.payload
     }
-   
   },
 })
 
 // Action creators are generated for each case reducer function
-export const { EmployeeRegisterDetails } = userSlice.actions
+export const { EmployeeRegisterDetails,RegisterClient,UpdateUserLoginSuccess,setClientLogin,superAdminLogin } = userSlice.actions
 
 export default userSlice.reducer 
